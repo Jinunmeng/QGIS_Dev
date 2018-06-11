783ec8ee3de4675bb7ea36ea254ba1b685f9657b
March <jinunmeng@live.com>
2018-06-11 13:55:44 +0800

    添加矢量数据、添加栅格数据、添加翻译文件

--- qgis_dev/qgis_dev/GeneratedFiles/ui_qgis_dev.h
+++ qgis_dev/qgis_dev/GeneratedFiles/ui_qgis_dev.h
@@ -15,6 +15,7 @@
 #include <QtWidgets/QButtonGroup>
 #include <QtWidgets/QHeaderView>
 #include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QToolBar>
@@ -25,28 +26,50 @@ QT_BEGIN_NAMESPACE
 class Ui_qgis_devClass
 {
 public:
-    QMenuBar *menuBar;
-    QToolBar *mainToolBar;
+    QAction *actionadd_vector;
+    QAction *actionadd_raster;
     QWidget *centralWidget;
+    QMenuBar *menuBar;
+    QMenu *menu;
+    QMenu *menu_2;
+    QMenu *menu_3;
+    QToolBar *mainToolBar;
     QStatusBar *statusBar;
 
     void setupUi(QMainWindow *qgis_devClass)
     {
         if (qgis_devClass->objectName().isEmpty())
             qgis_devClass->setObjectName(QStringLiteral("qgis_devClass"));
-        qgis_devClass->resize(600, 400);
-        menuBar = new QMenuBar(qgis_devClass);
-        menuBar->setObjectName(QStringLiteral("menuBar"));
-        qgis_devClass->setMenuBar(menuBar);
-        mainToolBar = new QToolBar(qgis_devClass);
-        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
-        qgis_devClass->addToolBar(mainToolBar);
+        qgis_devClass->resize(954, 668);
+        actionadd_vector = new QAction(qgis_devClass);
+        actionadd_vector->setObjectName(QStringLiteral("actionadd_vector"));
+        actionadd_raster = new QAction(qgis_devClass);
+        actionadd_raster->setObjectName(QStringLiteral("actionadd_raster"));
         centralWidget = new QWidget(qgis_devClass);
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
         qgis_devClass->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(qgis_devClass);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 954, 26));
+        menu = new QMenu(menuBar);
+        menu->setObjectName(QStringLiteral("menu"));
+        menu_2 = new QMenu(menuBar);
+        menu_2->setObjectName(QStringLiteral("menu_2"));
+        menu_3 = new QMenu(menuBar);
+        menu_3->setObjectName(QStringLiteral("menu_3"));
+        qgis_devClass->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(qgis_devClass);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        qgis_devClass->addToolBar(Qt::TopToolBarArea, mainToolBar);
         statusBar = new QStatusBar(qgis_devClass);
         statusBar->setObjectName(QStringLiteral("statusBar"));
         qgis_devClass->setStatusBar(statusBar);
+
+        menuBar->addAction(menu->menuAction());
+        menuBar->addAction(menu_2->menuAction());
+        menuBar->addAction(menu_3->menuAction());
+        menu_2->addAction(actionadd_vector);
+        menu_3->addAction(actionadd_raster);
 
         retranslateUi(qgis_devClass);
 
@@ -56,6 +79,11 @@ public:
     void retranslateUi(QMainWindow *qgis_devClass)
     {
         qgis_devClass->setWindowTitle(QApplication::translate("qgis_devClass", "qgis_dev", Q_NULLPTR));
+        actionadd_vector->setText(QApplication::translate("qgis_devClass", "add vector", Q_NULLPTR));
+        actionadd_raster->setText(QApplication::translate("qgis_devClass", "add raster", Q_NULLPTR));
+        menu->setTitle(QApplication::translate("qgis_devClass", "\346\226\207\344\273\266", Q_NULLPTR));
+        menu_2->setTitle(QApplication::translate("qgis_devClass", "\347\237\242\351\207\217", Q_NULLPTR));
+        menu_3->setTitle(QApplication::translate("qgis_devClass", "\346\240\205\346\240\274", Q_NULLPTR));
     } // retranslateUi
 
 };
---
+++ qgis_dev/qgis_dev/qgis_dev_zh.ts
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>qgis_devClass</name>
+    <message>
+        <location filename="qgis_dev.ui" line="14"/>
+        <source>qgis_dev</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qgis_dev.ui" line="28"/>
+        <source>文件</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qgis_dev.ui" line="33"/>
+        <source>矢量</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qgis_dev.ui" line="39"/>
+        <source>栅格</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qgis_dev.ui" line="58"/>
+        <source>add vector</source>
+        <translatorcomment>添加矢量</translatorcomment>
+        <translation type="unfinished">添加矢量</translation>
+    </message>
+    <message>
+        <location filename="qgis_dev.ui" line="63"/>
+        <source>add raster</source>
+        <translatorcomment>添加栅格</translatorcomment>
+        <translation type="unfinished">添加栅格</translation>
+    </message>
+</context>
+</TS>
